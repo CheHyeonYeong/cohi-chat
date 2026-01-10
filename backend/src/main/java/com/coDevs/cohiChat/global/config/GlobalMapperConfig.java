4d32a2873eb4af807c30b32a0b6f2560097f1a7a
Tarte <emprimula@gmail.com>
2026-01-11 00:41:03 +0900

    refactor: GlobalMapperConfig 추가

---
+++ backend/src/main/java/com/coDevs/cohiChat/global/config/GlobalMapperConfig.java
@@ -0,0 +1,13 @@
+package com.coDevs.cohiChat.global.config;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(
+	componentModel = "spring",
+	unmappedTargetPolicy = ReportingPolicy.IGNORE,
+	nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
+public interface GlobalMapperConfig {
+}
