386464d9ee8f2dfd29908fdcc63879b5dfc35313
Tarte <emprimula@gmail.com>
2026-01-11 22:38:16 +0900

    feat: UpdateMemberRequestDTO 수정

--- backend/src/main/java/com/coDevs/cohiChat/member/request/UpdateMemberRequestDTO.java
+++ backend/src/main/java/com/coDevs/cohiChat/member/request/UpdateMemberRequestDTO.java
@@ -1,6 +1,9 @@
 package com.coDevs.cohiChat.member.request;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
