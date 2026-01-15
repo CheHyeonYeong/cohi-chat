41b2e6a090cecaadb80cbfcca7b00a9a9fea81da
Tarte <emprimula@gmail.com>
2026-01-15 17:47:31 +0900

    Refactor: 불필요한 코드 삭제

--- backend/src/main/java/com/coDevs/cohiChat/global/security/jwt/JwtTokenProvider.java
+++ backend/src/main/java/com/coDevs/cohiChat/global/security/jwt/JwtTokenProvider.java
@@ -6,7 +6,6 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
-import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
