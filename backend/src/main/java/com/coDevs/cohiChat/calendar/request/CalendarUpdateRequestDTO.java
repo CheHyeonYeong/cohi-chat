1d08dc3c611581a21c81d3dc95f3b208c76a5fea
CheHyeonYeong <gusdudco6@naver.com>
2026-01-18 15:56:00 +0900

    style : 필요치 않은 주석 제거

--- backend/src/main/java/com/coDevs/cohiChat/calendar/request/CalendarUpdateRequestDTO.java
+++ backend/src/main/java/com/coDevs/cohiChat/calendar/request/CalendarUpdateRequestDTO.java
@@ -9,11 +9,9 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class CalendarUpdateRequestDTO {
 
