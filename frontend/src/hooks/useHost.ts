f81f0442148eadbe7affbf241d3c37778312ece3
Tarte <emprimula@gmail.com>
2026-01-20 18:07:19 +0900

    refactor: User 타입 -> MemberResponseDTO로 변경

--- frontend/src/hooks/useHost.ts
+++ frontend/src/hooks/useHost.ts
@@ -1,14 +1,14 @@
-import { useQuery } from '@tanstack/react-query';
-import { httpClient } from '~/libs/httpClient';
-import { User } from '~/types/user';
+import {useQuery} from '@tanstack/react-query';
+import {httpClient} from '~/libs/httpClient';
+import {MemberResponseDTO} from '~/types/user';
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
 export function useHosts() {
-    return useQuery<User[]>({
+    return useQuery<MemberResponseDTO[]>({
         queryKey: ['hosts'],
         queryFn: async () => {
-            const response = await httpClient<User[]>(`${API_URL}/account/hosts`);
+            const response = await httpClient<MemberResponseDTO[]>(`${API_URL}/account/hosts`);
             if (!response) {
                 throw new Error('Failed to fetch hosts');
             }
