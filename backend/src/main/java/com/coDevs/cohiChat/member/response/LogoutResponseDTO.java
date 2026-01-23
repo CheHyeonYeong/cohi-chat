d5d11ec4a3efa721f5a1d2916cebe0db6401b314
Tarte <emprimula@gmail.com>
2026-01-23 17:13:03 +0900

    fix: LogoutResponseDTO에 NoArgsConstructor 추가

--- backend/src/main/java/com/coDevs/cohiChat/member/response/LogoutResponseDTO.java
+++ backend/src/main/java/com/coDevs/cohiChat/member/response/LogoutResponseDTO.java
@@ -2,15 +2,15 @@ package com.coDevs.cohiChat.member.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class LogoutResponseDTO {
 	private String message;
 
 	public static LogoutResponseDTO success() {
-
 		return new LogoutResponseDTO("로그아웃 되었습니다.");
-
 	}
 }
