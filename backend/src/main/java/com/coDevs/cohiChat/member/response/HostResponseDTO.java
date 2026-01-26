9bea89178abac7f3b17472303615f3bfc9abcaad
Tarte <emprimula@gmail.com>
2026-01-26 21:55:49 +0900

    refactor: #73 HostResponseDTO record에서 class로 변경

--- backend/src/main/java/com/coDevs/cohiChat/member/response/HostResponseDTO.java
+++ backend/src/main/java/com/coDevs/cohiChat/member/response/HostResponseDTO.java
@@ -2,8 +2,24 @@ package com.coDevs.cohiChat.member.response;
 
 import com.coDevs.cohiChat.member.entity.Member;
 
-public record HostResponseDTO(String username, String displayName) {
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class HostResponseDTO {
+	private String username;
+	private String displayName;
+
 	public static HostResponseDTO from(Member member) {
-		return new HostResponseDTO(member.getUsername(), member.getDisplayName());
+		return HostResponseDTO.builder()
+			.username(member.getUsername())
+			.displayName(member.getDisplayName())
+			.build();
 	}
 }
