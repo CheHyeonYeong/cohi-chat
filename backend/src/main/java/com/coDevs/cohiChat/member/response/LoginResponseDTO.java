8b42988eabc09104d427c2005d7f3eb8cbd6266e
fru1tworld <fruitworld.planet@gmail.com>
2026-03-21 15:47:28 +0900

    refactor: #435 코드 모던화
    - LoginResponseDTO, SafeLoginResponseDTO를 Java 21 record로 전환
    - Lombok boilerplate 제거 (@Getter, @NoArgsConstructor, @AllArgsConstructor)
    - record accessor 스타일 적용 (getX() → x())

--- backend/src/main/java/com/coDevs/cohiChat/member/response/LoginResponseDTO.java
+++ backend/src/main/java/com/coDevs/cohiChat/member/response/LoginResponseDTO.java
@@ -1,27 +1,16 @@
 package com.coDevs.cohiChat.member.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
-public class LoginResponseDTO {
-
-	private String accessToken;
-	private long expiredInMinutes;
-	private String refreshToken;
-	private String username;
-	private String displayName;
-
+public record LoginResponseDTO(
+	String accessToken,
+	long expiredInMinutes,
+	String refreshToken,
+	String username,
+	String displayName
+) {
 	public SafeLoginResponseDTO toSafeResponse() {
-		return SafeLoginResponseDTO.builder()
-			.username(this.username)
-			.displayName(this.displayName)
-			.expiredInMinutes(this.expiredInMinutes)
-			.build();
+		return new SafeLoginResponseDTO(username, displayName, expiredInMinutes);
 	}
 }
--- backend/src/main/java/com/coDevs/cohiChat/member/response/SafeLoginResponseDTO.java
+++ backend/src/main/java/com/coDevs/cohiChat/member/response/SafeLoginResponseDTO.java
@@ -1,16 +1,7 @@
 package com.coDevs.cohiChat.member.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class SafeLoginResponseDTO {
-	private String username;
-	private String displayName;
-	private long expiredInMinutes;
-}
+public record SafeLoginResponseDTO(
+	String username,
+	String displayName,
+	long expiredInMinutes
+) {}
